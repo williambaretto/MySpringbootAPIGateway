133da7590fe7d95066ff91450cea9a73f878e851
wmbaretto <william.baretto@byjus.com>
2022-10-29 17:03:56 +0530

    all

---
+++ src/main/java/com/example/demo/api/gateway/MySpringApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/api/gateway/MySpringApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
